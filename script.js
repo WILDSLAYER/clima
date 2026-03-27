1fe8a65575489a8ac3cfd21361631607de18e6b3
WILDSLAYER <wilder.dev.velasquez@gmail.com>
2026-03-27 08:50:43 -0500

    creo los elementos que se van a mostrar en pantalla

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1. Seleccionamos los elementos del HTML que vamos a manipular
+
+const searchInput = document.getElementById('search-input');
+const loading = document.getElementById('loading');
+const errorDiv = document.getElementById('error');
+const weatherDiv = document.getElementById('weather');
+
+const cityNameEl = document.getElementById('city-name');
+const tempEl = document.getElementById('temp');
+const descriptionEl = document.getElementById('description');
+const humidityEl = document.getElementById('humidity');
+const windEl = document.getElementById('wind');
+const feelsLikeEl = document.getElementById('feels-like');
+const forecastContainer = document.getElementById('forecast');
